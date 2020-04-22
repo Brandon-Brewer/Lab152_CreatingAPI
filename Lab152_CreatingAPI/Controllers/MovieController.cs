92ebe92bf3cc3d77b8ed35ef2c11a9bfacd4236f
Brewer <brandonbrewer@quickenloans.com>
2020-04-22 07:01:51 -0400

    Fixed Bugs

---
+++ Lab152_CreatingAPI/Controllers/MovieController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab152_CreatingAPI.Models;
+using Lab152_CreatingAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab152_CreatingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private IDAL dal;
+
+        public MovieController(IDAL dalObject)
+        {
+            dal = dalObject;
+        }
+
+        [HttpDelete("{id}")]
+        public Object Delete(int id)
+        {
+            int result = dal.DeleteMovieById(id);
+
+            if (result > 0)
+            {
+                return new { success = true };
+            }
+            else
+            {
+                return new { success = false };
+            }
+        }
+
+        [HttpGet("{id}")]
+        public Movie GetSingleMovie(int id)
+        {
+            Movie mov = dal.GetMovieById(id);
+            return mov; //serialize the parameter into JSON and return an Ok (20x)
+        }
+
+        //[HttpGet]
+        //public IEnumerable<Movie> Get(string genres = null)
+        //{
+        //    if (genres == null)
+        //    {
+        //        IEnumerable<Movie> Movies = dal.GetMoviesAll();
+        //        return Movies; //serialize the parameter into JSON and return an Ok (20x)
+        //    }
+        //    else
+        //    {
+        //        IEnumerable<Movie> Movies = dal.GetMoviesByGenres(genres);
+        //        return Movies;
+        //    }
+        //}
+
+        //valid but superceded by Category controller
+        //[HttpGet("genres")]
+        //public string[] GetGenres()
+        //{
+        //    return dal.GetMovieGenres();
+        //}
+
+        [HttpPost]
+        public Object Post(Movie m)
+        {
+            int newId = dal.CreateMovie(m);
+
+            if (newId < 0)
+            {
+                return new { success = false };
+            }
+            return new { success = true, id = newId };
+        }
+    }
+}
---
+++ Lab152_CreatingAPI/Services/IDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab152_CreatingAPI.Models;
+
+namespace Lab152_CreatingAPI.Services
+{
+    public interface IDAL
+    {
+        int CreateMovie(Movie m);
+        int DeleteMovieById(int id);
+        Movie GetMovieById(int id);
+        IEnumerable<Movie> GetMoviesAll();
+        IEnumerable<Movie> GetMoviesByGenres(string genres);
+        //int UpdateMovietById(Movie mov);
+    }
+}
